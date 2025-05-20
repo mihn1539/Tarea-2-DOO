e9ddb595461b3ae43ee998c8df831732e35638d6
rodohub09 <superpro757@gmail.com>
2025-05-20 13:59:58 -0400

    Modificacion de las dependecias de pom.xml para implementacion de JUnit y creacion de directorio para los test

---
+++ src/test/java/org/example/AsistenciaTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AsistenciaTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+}
